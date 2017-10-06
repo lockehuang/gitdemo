0ef53f7559e6a1dedd56435284eccf167383010d
lockehuang <hx_huang126@126.com>
2017-10-06 11:40:22 +0800

    init

---
+++ MyGit/MyGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGit
+//
+//  Created by Mac on 2017/10/6.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGit/MyGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGit
+//
+//  Created by Mac on 2017/10/6.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
