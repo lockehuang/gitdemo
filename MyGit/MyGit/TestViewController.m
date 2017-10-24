5f87be7979143c3ea1c6510c04b58f20d50e84fd
lockehuang <hx_huang126@126.com>
2017-10-24 17:44:51 +0800

    loadData commit

--- MyGit/MyGit/TestViewController.m
+++ MyGit/MyGit/TestViewController.m
@@ -19,6 +19,10 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)loadData{
+    //测试
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
